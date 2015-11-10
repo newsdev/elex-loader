80b861366a02512f6d5696c4e08e0fe0a661d7d2
David Eads <davideads@gmail.com>
2015-11-10 13:43:46 -0500

    tweaking schema

--- load.sh
+++ load.sh
@@ -1,10 +1,11 @@
-dropdb --if-exists elex
-createdb elex
+#!/bin/bash
 
-psql elex -c "CREATE TABLE races (
+RACEDATE="2015-11-03"
+
+psql elex -c "DROP TABLE IF EXISTS races; CREATE TABLE races (
     description varchar,
     initialization_data boolean,
-    lastupdated varchar,
+    lastupdated date,
     national boolean,
     officeid varchar,
     officename varchar,
@@ -14,9 +15,39 @@ psql elex -c "CREATE TABLE races (
     racetypeid varchar,
     seatname varchar,
     seatnum varchar,
+    statename varchar,
+    statepostal char(2),
     test boolean,
     uncontested boolean
 );"
 
-elex init-races 2015-11-03 | psql elex -c "COPY races FROM stdin DELIMITER ',' CSV HEADER;"
+elex init-races '11-03-2015' | psql elex -c "COPY races FROM stdin DELIMITER ',' CSV HEADER;"
+
+psql elex -c "DROP TABLE IF EXISTS reporting_units; CREATE TABLE reporting_units(
+    description varchar,
+    fipscode char(5),
+    initialization_data boolean,
+    lastupdated date,
+    lastupdated_parsed date,
+    level varchar,
+    national varchar,
+    officeid varchar,
+    officename varchar,
+    precinctsreporting int,
+    precinctsreportingpct numeric,
+    precinctstotal int,
+    raceid varchar,
+    racetype varchar,
+    racetypeid varchar,
+    reportingunitid varchar,
+    reportingunitname varchar,
+    seatname varchar,
+    seatnum varchar,
+    statename varchar,
+    statepostal char(2),
+    test boolean,
+    uncontested varchar
+);"
+
+elex init-reporting-units $RACEDATE | psql elex -c "COPY reporting_units FROM stdin DELIMITER ',' CSV HEADER;"
 
