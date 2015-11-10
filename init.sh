4b37cfd021943366f6d16b4c8c9af4d79c019553
David Eads <davideads@gmail.com>
2015-11-10 17:27:54 -0500

    split up files and add basic override mechanism

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-RACEDATE="2015-11-03"
+source config.sh
 
+echo "Initialize races"
 psql elex -c "DROP TABLE IF EXISTS races; CREATE TABLE races (
     description varchar,
     initialization_data boolean,
     lastupdated date,
+    lastupdated_parsed varchar,
     national boolean,
     officeid varchar,
     officename varchar,
@@ -23,6 +25,7 @@ psql elex -c "DROP TABLE IF EXISTS races; CREATE TABLE races (
 
 elex init-races $RACEDATE | psql elex -c "COPY races FROM stdin DELIMITER ',' CSV HEADER;"
 
+echo "Initialize reporting units"
 psql elex -c "DROP TABLE IF EXISTS reporting_units; CREATE TABLE reporting_units(
     description varchar,
     fipscode char(5),
@@ -51,3 +54,34 @@ psql elex -c "DROP TABLE IF EXISTS reporting_units; CREATE TABLE reporting_units
 
 elex init-reporting-units $RACEDATE | psql elex -c "COPY reporting_units FROM stdin DELIMITER ',' CSV HEADER;"
 
+echo "Initialize candidates"
+psql elex -c "DROP TABLE IF EXISTS candidates; CREATE TABLE candidates(
+    ballotorder int,
+    candidateid int,
+    first varchar,
+    last varchar,
+    party varchar,
+    polid varchar,
+    polnum varchar
+);"
+
+elex init-candidates $RACEDATE | psql elex -c "COPY candidates FROM stdin DELIMITER ',' CSV HEADER;"
+
+echo "Initialize ballot positions"
+psql elex -c "DROP TABLE IF EXISTS ballot_positions; CREATE TABLE ballot_positions(
+    ballotorder int,
+    candidateid int,
+    description varchar,
+    last varchar,
+    polid varchar,
+    polnum varchar,
+    seatname varchar
+);"
+
+elex init-ballot-positions $RACEDATE | psql elex -c "COPY ballot_positions FROM stdin DELIMITER ',' CSV HEADER;"
+
+psql elex -c "CREATE TABLE name_overrides(
+    candidateid int,
+    first varchar,
+    last varchar
+);"
---
+++ update.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+source config.sh
+
+psql elex -c "DROP TABLE IF EXISTS results CASCADE; CREATE TABLE results(
+    ballotorder int,
+    candidateid int,
+    description varchar,
+    fipscode char(5),
+    first varchar,
+    incumbent boolean,
+    initialization_data boolean,
+    is_ballot_position boolean,
+    last varchar,
+    lastupdated date,
+    level varchar,
+    national boolean,
+    officeid varchar,
+    officename varchar,
+    party varchar,
+    polid varchar,
+    polnum varchar,
+    precinctsreporting int,
+    precinctsreportingpct numeric,
+    precinctstotal int,
+    raceid varchar,
+    racetype varchar,
+    racetypeid varchar,
+    reportingunitid varchar,
+    reportingunitname varchar,
+    seatname varchar,
+    seatnum varchar,
+    statename varchar,
+    statepostal varchar,
+    test boolean,
+    uncontested boolean,
+    votecount int,
+    votepct numeric,
+    winner boolean
+);"
+
+elex get-results $RACEDATE | psql elex -c "COPY results FROM stdin DELIMITER ',' CSV HEADER;"
+
+psql elex -c "DROP TABLE IF EXISTS name_overrides; CREATE TABLE name_overrides(
+    candidateid int,
+    first varchar,
+    last varchar
+);"
+
+psql elex -c "COPY name_overrides FROM '`pwd`/name_overrides.csv' DELIMITER ',' CSV HEADER;"
+
+psql elex -c "CREATE OR REPLACE VIEW npr_results as
+    SELECT n.last as display_last, n.first as display_first, r.* from results as r
+        LEFT JOIN name_overrides as n on r.candidateid = n.candidateid
+;"
