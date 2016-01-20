a57a37bfdec3b77172f2c72d8e8f36dbd3d65a8c
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-01-19 21:38:13 -0500

    Duh, need elex to work.

--- scripts/prd_test.sh
+++ scripts/prd_test.sh
@@ -16,6 +16,6 @@ if [[ -z "$AP_API_KEY" ]] ; then
 fi
 
 while [ 1 ]; do
-    /home/ubuntu/elex-loader/scripts/prd_test/update.sh $RACEDATE
+    workon elex-loader && /home/ubuntu/elex-loader/scripts/prd_test/update.sh $RACEDATE
     sleep 30
 done
