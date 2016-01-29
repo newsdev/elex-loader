3af4a6ed2b65167839b29bf399d5938db7dd5880
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-01-29 10:31:01 -0500

    Adds scripts for loading delegates. Upgrade elex version.

---
+++ scripts/dev/delegates.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# set RACEDATE from the first argument, if it exists
+if [[ ! -z $1 ]] ; then
+    RACEDATE=$1
+fi
+
+if [[ -z $RACEDATE ]] ; then
+    echo 'Provide a race date, such as 2016-02-01'
+    exit 1
+fi
+
+if [[ -z "$AP_API_KEY" ]] ; then
+    echo "Missing environmental variable AP_API_KEY. Try 'export AP_API_KEY=MY_API_KEY_GOES_HERE'."
+    exit 1
+fi
+
+date "+STARTED: %H:%M:%S"
+echo "------------------------------"
+
+psql elex_$RACEDATE -c "DROP TABLE IF EXISTS delegates CASCADE; CREATE TABLE delegates(
+    level varchar,
+    party_total integer,
+    superdelegates_count integer,
+    last varchar,
+    state varchar,
+    candidateid varchar,
+    party_need integer,
+    party varchar,
+    delegates_count integer,
+    id varchar
+);"
+
+elex delegates -t | psql elex_$RACEDATE -c "COPY delegates FROM stdin DELIMITER ',' CSV HEADER;"
+
+echo "------------------------------"
+date "+ENDED: %H:%M:%S"
