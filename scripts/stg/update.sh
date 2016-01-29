44ec509e462ad2a0b6797afebeaf33d520d6c921
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-01-29 13:35:43 -0500

    Error handling for bad data from AP.

--- scripts/stg/update.sh
+++ scripts/stg/update.sh
@@ -18,64 +18,88 @@ fi
 date "+STARTED: %H:%M:%S"
 echo "------------------------------"
 
-psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "DROP TABLE IF EXISTS results CASCADE; CREATE TABLE results(
-    id varchar,
-    unique_id varchar,
-    raceid varchar,
-    racetype varchar,
-    racetypeid varchar,
-    ballotorder int,
-    candidateid varchar,
-    description varchar,
-    electiondate varchar,
-    fipscode char(5),
-    first varchar,
-    incumbent bool,
-    initialization_data bool,
-    is_ballot_position bool,
-    last varchar,
-    lastupdated varchar,
-    level varchar,
-    national bool,
-    officeid varchar,
-    officename varchar,
-    party varchar,
-    polid varchar,
-    polnum varchar,
-    precinctsreporting int,
-    precinctsreportingpct numeric,
-    precinctstotal int,
-    reportingunitid varchar,
-    reportingunitname varchar,
-    runoff bool,
-    seatname varchar,
-    seatnum varchar,
-    statename varchar,
-    statepostal char(2),
-    test bool,
-    uncontested bool,
-    votecount int,
-    votepct numeric,
-    winner bool
-);"
+function drop_table
+{
+    psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "DROP TABLE IF EXISTS results CASCADE; CREATE TABLE results(
+        id varchar,
+        unique_id varchar,
+        raceid varchar,
+        racetype varchar,
+        racetypeid varchar,
+        ballotorder int,
+        candidateid varchar,
+        description varchar,
+        electiondate varchar,
+        fipscode char(5),
+        first varchar,
+        incumbent bool,
+        initialization_data bool,
+        is_ballot_position bool,
+        last varchar,
+        lastupdated varchar,
+        level varchar,
+        national bool,
+        officeid varchar,
+        officename varchar,
+        party varchar,
+        polid varchar,
+        polnum varchar,
+        precinctsreporting int,
+        precinctsreportingpct numeric,
+        precinctstotal int,
+        reportingunitid varchar,
+        reportingunitname varchar,
+        runoff bool,
+        seatname varchar,
+        seatnum varchar,
+        statename varchar,
+        statepostal char(2),
+        test bool,
+        uncontested bool,
+        votecount int,
+        votepct numeric,
+        winner bool
+    );"
+}
 
-elex results $RACEDATE -t | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "COPY results FROM stdin DELIMITER ',' CSV HEADER;"
+function get_results
+{
+    elex results $RACEDATE -t > results.csv
+}
 
-psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "CREATE OR REPLACE VIEW elex_races as
-   SELECT o.*, r.* from races as r
-       LEFT JOIN override_races as o on r.raceid = o.race_raceid
-;"
+function load_results
+{
+    cat results.csv | psql elex_$RACEDATE -c "COPY results FROM stdin DELIMITER ',' CSV HEADER;"
+}
 
-psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "CREATE OR REPLACE VIEW elex_results as
-   SELECT o.*, c.*, r.* from results as r
-       LEFT JOIN override_candidates as c on r.candidateid = c.candidate_candidateid
-       LEFT JOIN override_races as o on r.raceid = o.race_raceid
-;"
+function replace_views
+{
+    psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "CREATE OR REPLACE VIEW elex_races as
+       SELECT o.*, r.* from races as r
+           LEFT JOIN override_races as o on r.raceid = o.race_raceid
+    ;"
 
-psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "CREATE OR REPLACE VIEW elex_candidates as
-   SELECT c.*, r.* from candidates as r
-       LEFT JOIN override_candidates as c on r.candidateid = c.candidate_candidateid
-;"
+    psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "CREATE OR REPLACE VIEW elex_results as
+       SELECT o.*, c.*, r.* from results as r
+           LEFT JOIN override_candidates as c on r.candidateid = c.candidate_candidateid
+           LEFT JOIN override_races as o on r.raceid = o.race_raceid
+    ;"
+
+    psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "CREATE OR REPLACE VIEW elex_candidates as
+       SELECT c.*, r.* from candidates as r
+           LEFT JOIN override_candidates as c on r.candidateid = c.candidate_candidateid
+    ;"
+}
+
+if get_results; then
+    drop_table
+    load_results
+    replace_views
+    rm -rf results.csv
+else
+    echo "ERROR: Bad response from AP. No results loaded."
+    rm -rf results.csv
+fi
 
 echo "------------------------------"
 date "+ENDED: %H:%M:%S"
