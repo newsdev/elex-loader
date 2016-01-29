364f9a544128cc834cfc58ea1285b65d67192771
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-01-29 16:20:55 -0500

    Split out override initialization. Add an nyt_display_order field.

--- scripts/prd/init.sh
+++ scripts/prd/init.sh
@@ -109,30 +109,5 @@ psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "DROP TABLE IF EXISTS ballot_
 
 elex ballot-measures $RACEDATE | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "COPY ballot_positions FROM stdin DELIMITER ',' CSV HEADER;"
 
-echo "Create candidate overrides table"
-psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "DROP TABLE IF EXISTS override_candidates CASCADE; CREATE TABLE override_candidates(
-    candidate_candidateid varchar,
-    nyt_candidate_name varchar,
-    nyt_candidate_important bool,
-    nyt_candidate_description text,
-    nyt_races integer[],
-    nyt_winner bool
-);"
-
-echo "Create race overrides table"
-psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "DROP TABLE IF EXISTS override_races CASCADE; CREATE TABLE override_races(
-    nyt_race_preview text,
-    nyt_race_result_description text,
-    nyt_delegate_allocation text,
-    report bool,
-    report_description text,
-    race_raceid varchar,
-    nyt_race_name varchar,
-    nyt_race_description text,
-    accept_ap_calls bool,
-    nyt_called bool,
-    nyt_race_important bool
-);"
-
 echo "------------------------------"
 date "+ENDED: %H:%M:%S"
