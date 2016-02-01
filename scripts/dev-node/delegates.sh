de376cf2a37b747feb94806de05c675ce152e4ff
Wilson Andrews <unc.wilson@gmail.com>
2016-02-01 11:01:24 -0500

    Add dev node paths

---
+++ scripts/dev-node/delegates.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [[ ! -z $1 ]] ; then
+    RACEDATE=$1
+fi
+
+
+if [[ -z $RACEDATE ]] ; then
+    echo 'Provide a race date, such as 2016-02-01'
+    exit 1
+fi
+
+
+if [[ -z "$AP_API_KEY" ]] ; then
+    echo "Missing environmental variable AP_API_KEY. Try 'export AP_API_KEY=MY_API_KEY_GOES_HERE'."
+    exit 1
+fi
+
+
+date "+STARTED: %H:%M:%S"
+echo "------------------------------"
+
+
+cat node_modules/fields/delegates.txt | psql elex_$RACEDATE
+elex delegates -t | psql elex_$RACEDATE -c "COPY delegates FROM stdin DELIMITER ',' CSV HEADER;"
+
+
+echo "------------------------------"
+date "+ENDED: %H:%M:%S"
