8cfe68d1aa3b71c94ada9215ab82f9208fdf5e19
Wilson Andrews <unc.wilson@gmail.com>
2016-02-01 14:41:27 -0500

    Prd node

---
+++ scripts/prd-node/candidate_overrides.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [[ ! -z $1 ]] ; then
+    RACEDATE=$1
+fi
+
+if [[ -z $RACEDATE ]] ; then
+    echo 'Provide a race date, such as 2016-02-01'
+    exit 1
+fi
+
+if [[ -z "$AP_API_KEY" ]] ; then
+    echo "Missing environmental variable AP_API_KEY. Try 'export AP_API_KEY=MY_API_KEY_GOES_HERE'."
+    exit 1
+fi
+
+date "+STARTED: %H:%M:%S"
+echo "------------------------------"
+
+echo "Create candidate overrides table"
+cat node_modules/elex-loader/fields/candidate_overrides.txt | psql elex_$RACEDATE
+cat node_modules/elex-loader/fields/elex_candidates.txt | psql elex_$RACEDATE
+
+echo "------------------------------"
+date "+ENDED: %H:%M:%S"
