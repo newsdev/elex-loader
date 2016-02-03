3d72bc26865948712feb830684104a339035f386
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-02-03 11:52:28 -0500

    HAHAHAHAH 600

--- scripts/stg/daemon.sh
+++ scripts/stg/daemon.sh
@@ -18,5 +18,5 @@ fi
 while [ 1 ]; do
     source /etc/environment && source /home/ubuntu/.virtualenvs/elex-loader/bin/activate && /bin/bash /home/ubuntu/elex-loader/scripts/stg/update.sh $RACEDATE
     export NODE_ENV="staging" && cd /home/ubuntu/election-2016/ && npm run post-update $RACEDATE
-    sleep 600
+    sleep 30
 done
