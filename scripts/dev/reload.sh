5a3cefae8358664a00a42b71a940f42191e792d5
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-02-05 15:43:44 -0500

    Adds a dev reload script

--- scripts/dev/reload.sh
+++ scripts/dev/reload.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ./scripts/dev/init.sh
 ./scripts/dev/race_overrides.sh
 ./scripts/dev/candidate_overrides.sh
---
+++ scripts/stg/reload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./home/ubuntu/elex-loader/scripts/dev/init.sh
+./home/ubuntu/elex-loader/scripts/dev/race_overrides.sh
+./home/ubuntu/elex-loader/scripts/dev/candidate_overrides.sh
+./home/ubuntu/elex-loader/scripts/dev/update.sh
