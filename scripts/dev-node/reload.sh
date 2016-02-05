5bacb43ea69f6526b33b745a90225abf4dc06251
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-02-05 15:51:53 -0500

    Adds convenience script

---
+++ scripts/dev-node/reload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./node_modules/elex-loader/scripts/dev-node/init.sh
+./node_modules/elex-loader/scripts/dev-node/race_overrides.sh
+./node_modules/elex-loader/scripts/dev-node/candidate_overrides.sh
+./node_modules/elex-loader/scripts/dev-node/update.sh
