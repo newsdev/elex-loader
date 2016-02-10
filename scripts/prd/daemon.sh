9368d0f3c41ae7e5684980b60ff121c3c7939716
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-02-10 14:56:06 -0500

    Node env is actually specified in /etc/environment

--- scripts/prd/daemon.sh
+++ scripts/prd/daemon.sh
@@ -11,7 +11,6 @@ fi
 
 source '/etc/environment'
 source '/home/ubuntu/.virtualenvs/elex-loader/bin/activate'
-export NODE_ENV='production'
 cd '/home/ubuntu/election-2016/' || exit
 
 while true; do
--- scripts/stg/daemon.sh
+++ scripts/stg/daemon.sh
@@ -11,7 +11,6 @@ fi
 
 source '/etc/environment'
 source '/home/ubuntu/.virtualenvs/elex-loader/bin/activate'
-export NODE_ENV='staging'
 cd '/home/ubuntu/election-2016/' || exit
 
 while true; do
