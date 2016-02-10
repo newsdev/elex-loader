9f1e0ac50d8444d8f79b61fd3b77fc4864ba3c8a
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-02-10 11:19:51 -0500

    One more time.

--- scripts/stg/daemon.sh
+++ scripts/stg/daemon.sh
@@ -14,8 +14,8 @@ source '/home/ubuntu/.virtualenvs/elex-loader/bin/activate'
 cd '/home/ubuntu/election-2016/' || exit
 
 while true; do
-    ./home/ubuntu/elex-loader/scripts/stg/update.sh "$RACEDATE"
-    ./home/ubuntu/elex-loader/scripts/stg/delegates.sh "$RACEDATE"
+    /home/ubuntu/elex-loader/scripts/stg/update.sh "$RACEDATE"
+    /home/ubuntu/elex-loader/scripts/stg/delegates.sh "$RACEDATE"
     npm run post-update "$RACEDATE"
     sleep 30
 done
