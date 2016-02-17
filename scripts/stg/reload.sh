3ec2b2e7341ed1f1f62133e5a2257e9abc48088a
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-02-17 11:22:45 -0500

    Reload should also load candidates

--- scripts/stg/reload.sh
+++ scripts/stg/reload.sh
@@ -18,3 +18,5 @@ fi
 ./scripts/stg/race_overrides.sh $RACEDATE
 ./scripts/stg/candidate_overrides.sh $RACEDATE
 ./scripts/stg/update.sh $RACEDATE
+
+workon elex-admin-$RACEDATE && python /home/ubuntu/elex-admin/elex_admin/initialize_candidates.py
