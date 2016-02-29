aadf5aa58fee9af332dbd4d45d10f5e8380f38fc
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-02-29 18:12:20 -0500

    LAST PIECE

--- scripts/prd/init.sh
+++ scripts/prd/init.sh
@@ -53,6 +53,11 @@ function initialize_data {
     elex delegates | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "COPY delegates FROM stdin DELIMITER ',' CSV HEADER;"
 
     elex results $RACEDATE --results-level district | grep 'Z,district,\|,lastupdated,level,national,' | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "COPY results FROM stdin DELIMITER ',' CSV HEADER;"
+
+    cat /home/ubuntu/elex-loader/fields/elex_races.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
+    cat /home/ubuntu/elex-loader/fields/elex_candidates.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
+    cat /home/ubuntu/elex-loader/fields/elex_results.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
+
 }
 
 function start_services {
