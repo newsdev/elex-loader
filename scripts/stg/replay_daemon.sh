1c793912b34e80830cbe14fd275406c205e561d7
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-03-22 15:56:46 -0400

    Add districts back

--- scripts/stg/replay_daemon.sh
+++ scripts/stg/replay_daemon.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-. /home/ubuntu/elex-loader/scripts/prd/_delegates.sh
-. /home/ubuntu/elex-loader/scripts/prd/_districts.sh
-. /home/ubuntu/elex-loader/scripts/prd/_overrides.sh
-. /home/ubuntu/elex-loader/scripts/prd/_post.sh
-. /home/ubuntu/elex-loader/scripts/prd/_pre.sh
-. /home/ubuntu/elex-loader/scripts/prd/_results.sh
-. /home/ubuntu/elex-loader/scripts/prd/_views.sh
+. /home/ubuntu/elex-loader/scripts/stg/_delegates.sh
+. /home/ubuntu/elex-loader/scripts/stg/_districts.sh
+. /home/ubuntu/elex-loader/scripts/stg/_overrides.sh
+. /home/ubuntu/elex-loader/scripts/stg/_post.sh
+. /home/ubuntu/elex-loader/scripts/stg/_pre.sh
+. /home/ubuntu/elex-loader/scripts/stg/_replay_results.sh
+. /home/ubuntu/elex-loader/scripts/stg/_views.sh
 
 . /etc/environment
 
@@ -19,7 +19,7 @@ if [ -f /tmp/elex_loader_timeout.sh ]; then
 fi
 
 if [[ -z $ELEX_LOADER_TIMEOUT ]] ; then
-    ELEX_LOADER_TIMEOUT=15
+    ELEX_LOADER_TIMEOUT=60
 fi
 
 for (( i=1; i<100000; i+=1 )); do
