d3a6d182c2c7b97fc398f336254eb61d40fc386d
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-09-13 20:38:12 -0400

    Remove parallelizatoin

--- scripts/prd/daemon.sh
+++ scripts/prd/daemon.sh
@@ -32,10 +32,8 @@ for (( i=1; i<100000; i+=1 )); do
     pre
     set_db_tables
 
-    local_results & PIDLOCAL=$!
-    national_results & PIDNATIONAL=$!
-    wait $PIDLOCAL
-    wait $PIDNATIONAL
+    local_results
+    national_results
 
     views
     post
