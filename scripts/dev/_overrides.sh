0dc9de59afb870051ca3e25ee6324c1b03296e99
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-11-04 14:41:00 -0400

    Prepping stg

--- scripts/dev/_overrides.sh
+++ scripts/dev/_overrides.sh
@@ -1,6 +1,4 @@
 function overrides {
     cat fields/candidate_overrides.txt | psql elex_$RACEDATE
-    # cat $OVERRIDE_DIR/$RACEDATE'_override_candidates.csv' | psql elex_$RACEDATE -c "COPY override_candidates FROM stdin DELIMITER ',' CSV HEADER;"
     cat fields/race_overrides.txt | psql elex_$RACEDATE
-    # cat $OVERRIDE_DIR/$RACEDATE'_override_races.csv' | psql elex_$RACEDATE -c "COPY override_races FROM stdin DELIMITER ',' CSV HEADER;"
 }
--- scripts/stg/_views.sh
+++ scripts/stg/_views.sh
@@ -1,5 +1,4 @@
 function views {
     cat /home/ubuntu/elex-loader/fields/elex_races.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
-    cat /home/ubuntu/elex-loader/fields/elex_candidates.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
     cat /home/ubuntu/elex-loader/fields/elex_results.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
 }
