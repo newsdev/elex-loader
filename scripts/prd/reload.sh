6106b8cd5fb0b226fb0b1bf13e77ee7a999fb402
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-11-05 10:02:09 -0400

    Sigh

--- scripts/prd/reload.sh
+++ scripts/prd/reload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-. /home/ubuntu/elex-loader/scripts/stg/_districts.sh
-. /home/ubuntu/elex-loader/scripts/stg/_overrides.sh
-. /home/ubuntu/elex-loader/scripts/stg/_pre.sh
-. /home/ubuntu/elex-loader/scripts/stg/_results.sh
-. /home/ubuntu/elex-loader/scripts/stg/_views.sh
+. /home/ubuntu/elex-loader/scripts/prd/_districts.sh
+. /home/ubuntu/elex-loader/scripts/prd/_overrides.sh
+. /home/ubuntu/elex-loader/scripts/prd/_pre.sh
+. /home/ubuntu/elex-loader/scripts/prd/_results.sh
+. /home/ubuntu/elex-loader/scripts/prd/_views.sh
 
 if [[ ! -z $1 ]] ; then 
     RACEDATE=$1 
