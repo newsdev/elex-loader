f8b4a75c1982db45b81a256eb83eee9d75cadd8f
Wilson Andrews <unc.wilson@gmail.com>
2017-06-16 15:06:13 -0400

    Update csv script

--- scripts/stg/update_csv.sh
+++ scripts/stg/update_csv.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-. /home/ubuntu/elex-loader/scripts/stg/_post.sh
 . /home/ubuntu/elex-loader/scripts/stg/_pre.sh
 . /home/ubuntu/elex-loader/scripts/stg/_results.sh
-. /home/ubuntu/elex-loader/scripts/stg/_views.sh
 
 if [[ ! -z $1 ]] ; then
     RACEDATE=$1
@@ -11,6 +9,8 @@ fi
 if [[ -z $AP_API_BASE_URL ]] ; then
     AP_API_BASE_URL="http://api.ap.org/v2/"
 fi
+
+echo "----------- UPDATE CSV -----------"
 
 TIMESTAMP=$(date +"%s")
 
