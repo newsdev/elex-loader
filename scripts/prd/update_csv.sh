08410ae071bb14c02602b00983c40378caab8ef5
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-06-20 13:47:02 -0400

    Prd version

---
+++ scripts/prd/update_csv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+. /home/ubuntu/elex-loader/scripts/prd/_pre.sh
+. /home/ubuntu/elex-loader/scripts/prd/_results.sh
+. /etc/environment
+
+if [[ ! -z $1 ]] ; then
+    RACEDATE=$1
+fi
+
+if [[ -z $AP_API_BASE_URL ]] ; then
+    AP_API_BASE_URL="http://api.ap.org/v2/"
+fi
+
+echo "----------- UPDATE CSV -----------"
+
+TIMESTAMP=$(date +"%s")
+
+pre
+# set_temp_tables
+
+echo "0" > /tmp/elex_error.txt
+
+echo $AP_API_BASE_URL"/elections/$RACEDATE?apiKey=$AP_NAT_KEY&format=json&level=ru"
+
+results_csv
+
+echo "Results time elapsed:" $SECONDS"s"
+
+echo "0" > /tmp/elex_error.txt
+
+exit 0
